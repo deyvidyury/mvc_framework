322e427d78174b26c8a42a0a1ad8fd81848c771f
Deyvid Lopes <deyvidyury@yahoo.com.br>
2024-09-19 14:45:59 -0300

    Auth controller init

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+use app\controllers\SiteController;
+use app\core\Application;
+
+require_once __dir__ . "./../vendor/autoload.php";
+
+$app = new Application(dirname(__DIR__));
+
+$app->router->get('/',[SiteController::class, 'home']);
+$app->router->get('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact',[SiteController::class, 'handleContact']);
+
+$app->run();
